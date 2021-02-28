9e1dba0871cf2d2bcf72c13bb347c83ddefcb8eb
Harish Kumar <g.harishkumar1998@gmail.com>
2021-02-28 14:08:38 +0530

    Add files via upload

---
+++ face_reco_webcam_rpi.py
@@ -0,0 +1,67 @@
+from os import system
+import numpy as np
+import face_recognition as fr
+import cv2
+#from gpiozero import LED
+#from gpiozero.pins.pigpio import PiGPIOFactory
+
+video_capture = cv2.VideoCapture(0)
+
+image = fr.load_image_file("harish.jpg")
+face_encoding = fr.face_encodings(image)[0]
+
+known_face_encondings = [face_encoding]
+known_face_names = ["harish"]
+
+#factory = PiGPIOFactory(host='192.168.225.135')
+#led = LED(17, pin_factory=factory)
+
+while True: 
+    ret, frame = video_capture.read()
+
+    rgb_frame = frame[:, :, ::-1]
+
+    face_locations = fr.face_locations(rgb_frame)
+    face_encodings = fr.face_encodings(rgb_frame, face_locations)
+    
+    authentication = False
+
+    for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+
+        matches = fr.compare_faces(known_face_encondings, face_encoding)
+
+        name = "Unknown"
+
+        face_distances = fr.face_distance(known_face_encondings, face_encoding)
+
+        best_match_index = np.argmin(face_distances)
+        if matches[best_match_index]:
+            authentication = True
+            name = known_face_names[best_match_index]
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
+            cv2.rectangle(frame, (left, bottom -35), (right, bottom), (0, 255, 0), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+        else:
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+            cv2.rectangle(frame, (left, bottom -35), (right, bottom), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+            
+    if authentication:
+        system('clear')
+        print("ok")
+        #led.on()
+    else:
+        system('clear')
+        print("U cannot start engine")
+        #led.off()
+
+    cv2.imshow('Webcam_facerecognition', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+video_capture.release()
+cv2.destroyAllWindows()
+
