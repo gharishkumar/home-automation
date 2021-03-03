ed71000481b2697dcbb5c4f0eac482b09f56ff9e
Harish Kumar <g.harishkumar1998@gmail.com>
2021-03-03 07:19:08 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,3 @@
+sudo apt update
+sudo apt install -y python3-opencv  cmake libatlas-base-dev libhdf5-dev libhdf5-serial-dev libatlas-base-dev libjasper-dev  libqtgui4  libqt4-test 
+sudo pip3 --no-cache-dir install face-recognition keyboard python-telegram-bot
