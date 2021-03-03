c6faf27350a9d73ecb36dc5faeb75ec5ec76ca77
Harish Kumar <g.harishkumar1998@gmail.com>
2021-03-03 17:21:51 +0530

    ⬆️ correction and best practices

--- capture_image.py
+++ capture_image.py
@@ -1,11 +1,25 @@
 import cv2
 video_capture = cv2.VideoCapture(0)
 input('Ready press enter to capture image of 1st person')
-ret, frame = video_capture.read()
+while True:
+    ret, frame = video_capture.read()
+    if ret:
+        break
 cv2.imwrite('Person1_IMG.jpg', frame)
+video_capture.release()
+video_capture = cv2.VideoCapture(0)
 input('Ready press enter to capture image of 2nd person')
-ret, frame = video_capture.read()
+while True:
+    ret, frame = video_capture.read()
+    if ret:
+        break
 cv2.imwrite('Person2_IMG.jpg', frame)
+video_capture.release()
+video_capture = cv2.VideoCapture(0)
 input('Ready press enter to capture image of 3rd person')
-ret, frame = video_capture.read()
+while True:
+    ret, frame = video_capture.read()
+    if ret:
+        break
 cv2.imwrite('Person3_IMG.jpg', frame)
+video_capture.release()
