9412012a75e022cd91bfd20cc6612f0d6dd56f8e
Harish Kumar <g.harishkumar1998@gmail.com>
2021-03-19 17:50:50 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 sudo apt update
 sudo apt install -y python3-opencv  cmake libatlas-base-dev libhdf5-dev libhdf5-serial-dev libatlas-base-dev libjasper-dev  libqtgui4  libqt4-test 
 sudo pip3 --no-cache-dir install face-recognition keyboard python-telegram-bot
-sudo chmod +x $1/run.sh
 cd $1
+sudo chmod +x run.sh
 sudo crontab crontab.txt
